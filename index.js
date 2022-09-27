24e47fdfc7be2258bbbfe0e055a49a41e51ccf46
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-09-27 13:47:09 +0530

    All the file are added and the website is ready to use

---
+++ index.js
@@ -0,0 +1,10 @@
+let navlink = document.getElementById("nav-link");
+        function showmenu(){
+            navlink.style.display = "block";
+            // transition: 0.5s;
+        }
+
+
+        function hidemenu(){
+            navlink.style.display = "none";
+        }
